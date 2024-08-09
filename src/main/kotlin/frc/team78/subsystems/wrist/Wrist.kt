a93f8439ba6c0efca616ffacedd47d3259b014cf
Tim Winters <tim.winters@bevi.co>
2024-08-09 17:27:27 -0400

    Convert wrist to falcon

--- src/main/kotlin/frc/team78/subsystems/wrist/Wrist.kt
+++ src/main/kotlin/frc/team78/subsystems/wrist/Wrist.kt
@@ -1,16 +1,17 @@
 package frc.team78.subsystems.wrist
 
-import com.revrobotics.CANSparkBase
-import com.revrobotics.CANSparkLowLevel
-import com.revrobotics.CANSparkMax
-import com.revrobotics.SparkLimitSwitch
+import com.ctre.phoenix6.configs.TalonFXConfiguration
+import com.ctre.phoenix6.controls.PositionVoltage
+import com.ctre.phoenix6.hardware.TalonFX
+import com.ctre.phoenix6.signals.GravityTypeValue
+import com.ctre.phoenix6.signals.NeutralModeValue
+import com.reduxrobotics.sensors.canandmag.Canandmag
 import edu.wpi.first.networktables.NetworkTableInstance
 import edu.wpi.first.units.Units.Volts
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
 import edu.wpi.first.wpilibj2.command.Commands
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine
-import frc.team78.lib.setStatusRates
 
 object Wrist : SubsystemBase("Wrist") {
 
@@ -22,62 +23,77 @@ object Wrist : SubsystemBase("Wrist") {
 
     private const val MOTOR_CAN_ID = 13
     private const val K_P = 0.03
-    private const val FORWARD_SOFT_LIMIT = 60f
-    private const val REVERSE_SOFT_LIMIT = 5f
+    private const val FORWARD_SOFT_LIMIT = 60.0
+    private const val REVERSE_SOFT_LIMIT = 5.0
     private const val STOW_ANGLE = 55.0
     private const val AMP_ANGLE = 20.0
 
     private val positionNtPub =
         NetworkTableInstance.getDefault().getTable("wrist").getDoubleTopic("position").publish()
+    private val config =
+        TalonFXConfiguration().apply {
+            this.Slot0.kP = K_P
+            this.Slot0.GravityType = GravityTypeValue.Arm_Cosine
+
+            this.ClosedLoopGeneral.ContinuousWrap = true
+            // Times 360 to convert to degrees
+            this.Feedback.SensorToMechanismRatio = 360 * 108.0
+
+            this.SoftwareLimitSwitch.ForwardSoftLimitThreshold = FORWARD_SOFT_LIMIT
+            this.SoftwareLimitSwitch.ForwardSoftLimitEnable = true
+            this.SoftwareLimitSwitch.ReverseSoftLimitThreshold = REVERSE_SOFT_LIMIT
+            this.SoftwareLimitSwitch.ReverseSoftLimitEnable = true
+        }
+    private val absEncoder = Canandmag(0)
+
     private val motor =
-        CANSparkMax(MOTOR_CAN_ID, CANSparkLowLevel.MotorType.kBrushless).apply {
-            restoreFactoryDefaults()
-            idleMode = CANSparkBase.IdleMode.kCoast
-            pidController.setFeedbackDevice(absoluteEncoder)
-            pidController.positionPIDWrappingEnabled = true
-            pidController.positionPIDWrappingMinInput = 0.0
-            pidController.positionPIDWrappingMaxInput = 360.0
-            pidController.p = K_P
-
-            setSoftLimit(CANSparkBase.SoftLimitDirection.kForward, FORWARD_SOFT_LIMIT)
-            setSoftLimit(CANSparkBase.SoftLimitDirection.kReverse, REVERSE_SOFT_LIMIT)
-
-            setStatusRates(period0 = 20, period4 = 20)
+        TalonFX(MOTOR_CAN_ID).apply {
+            this.configurator.apply(config)
+            // Start the wrist in coast mode when the robot turns on
+            this.setNeutralMode(NeutralModeValue.Coast)
+            // Seed the position with the absolute encoder
+            this.setPosition(absEncoder.position, 1.0)
+            this.position.setUpdateFrequency(100.0)
+            this.velocity.setUpdateFrequency(100.0)
+            this.optimizeBusUtilization()
         }
 
-    private val hardLimitFwd = motor.getForwardLimitSwitch(SparkLimitSwitch.Type.kNormallyOpen)
+    private val control =
+        PositionVoltage(
+            STOW_ANGLE,
+            0.0,
+            true,
+            0.0,
+            0,
+            false,
+            false,
+            false,
+        )
 
-    private val encoder =
-        motor.absoluteEncoder.apply {
-            positionConversionFactor = 360.0
-            inverted = true
-        }
-
-    fun setTarget(target: Double) =
-        motor.pidController.setReference(target, CANSparkBase.ControlType.kPosition)
+    fun setTarget(target: Double) = motor.setControl(control.withPosition(target))
 
     private fun setTargetCommand(target: Double) =
         runOnce { setTarget(target) }.withName("setTarget($target)")
 
     val position
-        get() = encoder.position
+        get() = motor.position.value
 
     fun stow() = setTargetCommand(STOW_ANGLE)
 
     fun ampPosition() = setTargetCommand(AMP_ANGLE)
 
     fun coast() =
-        runOnce { motor.idleMode = CANSparkBase.IdleMode.kCoast }
+        runOnce { motor.setNeutralMode(NeutralModeValue.Coast) }
             .ignoringDisable(true)
             .withName("Coast Wrist")
 
     fun brake() =
-        runOnce { motor.idleMode = CANSparkBase.IdleMode.kBrake }
+        runOnce { motor.setNeutralMode(NeutralModeValue.Brake) }
             .ignoringDisable(true)
             .withName("Brake Wrist")
 
     override fun periodic() {
-        positionNtPub.set(encoder.position)
+        positionNtPub.set(position)
     }
 
     private val sysIdRoutine =
@@ -93,15 +109,17 @@ object Wrist : SubsystemBase("Wrist") {
 
     fun runSysId() =
         Commands.sequence(
-            sysIdRoutine.dynamic(SysIdRoutine.Direction.kForward).until { hardLimitFwd.isPressed },
+            sysIdRoutine.dynamic(SysIdRoutine.Direction.kForward).until {
+                motor.fault_ForwardSoftLimit.value
+            },
             sysIdRoutine.dynamic(SysIdRoutine.Direction.kReverse).until {
-                motor.getFault(CANSparkBase.FaultID.kSoftLimitRev)
+                motor.fault_ReverseSoftLimit.value
             },
             sysIdRoutine.quasistatic(SysIdRoutine.Direction.kForward).until {
-                hardLimitFwd.isPressed
+                motor.fault_ForwardSoftLimit.value
             },
             sysIdRoutine.quasistatic(SysIdRoutine.Direction.kReverse).until {
-                motor.getFault(CANSparkBase.FaultID.kSoftLimitRev)
+                motor.fault_ReverseSoftLimit.value
             },
         )
 }
